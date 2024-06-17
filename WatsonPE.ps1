59ec2b3318ca5315a1e79697644bb923ce30cad4
G0urmet <60777262+G0urmetD@users.noreply.github.com>
2024-06-17 15:23:42 +0200

    Update WatsonPE.ps1

--- WatsonPE.ps1
+++ WatsonPE.ps1
@@ -64,7 +64,7 @@ if($help) {
     Write-Output "
         -h, -help               Show help function.
         -a, -all                Run all checks.
-        -l, -light              Run light checks.
+        -l, -light              Run light checks (quick wins).
     "
     Write-Host "==================== { Colors } ===================="
     Write-Host -ForegroundColor YELLOW "        [YELLOW]" -NoNewline
@@ -171,190 +171,308 @@ function CheckSystemInfo {
 }
 
 function Invoke-WorkstationChecks {
-    Write-Host "==================== { Computer Information } ====================" -ForegroundColor Blue
-    Write-Host "==============================================================" -ForegroundColor Blue
-    Write-Host "==================== { KernelInformation" -ForegroundColor Blue
-    KernelInformation
-
-    Write-Host ""
-    Write-Host "==================== { Domain joined test & domain information" -ForegroundColor Blue
-    Test-DomainJoinStatus
-
-    Write-Host ""
-    Write-Host "==================== { Domain joined test & domain information" -ForegroundColor Blue
-    AntiVirusDetection
-
-    Write-Host ""
-    Write-Host "==================== { Windows Installed Hotfixes" -ForegroundColor Blue
-    WindowsHotfixes
-
-    Write-Host ""
-    Write-Host "==================== { Windows Hotfix History" -ForegroundColor Blue
-    WindowsHotfixHistory
-
-    Write-Host ""
-    Write-Host "==================== { Audit, WEF and LAPS" -ForegroundColor Blue
-    PSAuditWEFLAPS
-
-    Write-Host ""
-    Write-Host "==================== { LSA Protection check" -ForegroundColor Blue
-    LSAProtection
-
-    Write-Host ""
-    Write-Host "==================== { Credential Guard check" -ForegroundColor Blue
-    CredentialGuard
-
-    Write-Host ""
-    Write-Host "==================== { WDigest" -ForegroundColor Blue
-    WDigest
-
-    Write-Host ""
-    Write-Host "==================== { Cached Winlogon Credentials" -ForegroundColor Blue
-    CachedWinlogonCredentials
-
-    Write-Host ""
-    Write-Host "==================== { Environment Variables" -ForegroundColor Blue
-    EnvironmentVariables
-
-    Write-Host ""
-    Write-Host "==================== { UAC Settings/Bypass" -ForegroundColor Blue
-    UACSettings
-
-    Write-Host ""
-    Write-Host "==================== { Spooler/PrintNightmare" -ForegroundColor Blue
-    Spooler
-
-    Write-Host ""
-    Write-Host "==================== { Weak registry settings" -ForegroundColor Blue
-    WeakRegistryKeyPermissions
-
-    Write-Host ""
-    Write-Host "==================== { Unattended Files" -ForegroundColor Blue
-    UnattendedFiles
-
-    Write-Host ""
-    Write-Host "==================== { Unattended Files" -ForegroundColor Blue
-    CheckDirectoryPermissions
-
-    Write-Host ""
-    Write-Host "==================== { User Information } ====================" -ForegroundColor Blue
-    Write-Host "==============================================================" -ForegroundColor Blue
     
-    Write-Host ""
-    Write-Host "==================== { Local Groups and directory access" -ForegroundColor Blue
-    UserInformation
+    param(
+        [Parameter(HelpMessage = "Switch parameter to run all local priv checks.")]
+        [Alias('a')]
+        [switch]$all,
 
-    Write-Host ""
-    Write-Host "==================== { Net accounts Information" -ForegroundColor Blue
-    NetAccountsInfo
+        [Parameter(HelpMessage = "Switch parameter run quick wins.")]
+        [Alias('l')]
+        [switch]$light
+    )
 
-    Write-Host ""
-    Write-Host "==================== { Remote Sessions" -ForegroundColor Blue
-    RemoteSessions
+    if($all) {
+        Write-Host "==================== { Computer Information } ====================" -ForegroundColor Blue
+        Write-Host "==============================================================" -ForegroundColor Blue
+        Write-Host "==================== { KernelInformation" -ForegroundColor Blue
+        KernelInformation
 
-    Write-Host ""
-    Write-Host "==================== { Current Sessions" -ForegroundColor Blue
-    CurrentSessions
+        Write-Host ""
+        Write-Host "==================== { Domain joined test & domain information" -ForegroundColor Blue
+        Test-DomainJoinStatus
 
-    Write-Host ""
-    Write-Host "==================== { AlwaysInstallElevated" -ForegroundColor Blue
-    AlwaysInstallElevated
+        Write-Host ""
+        Write-Host "==================== { Domain joined test & domain information" -ForegroundColor Blue
+        AntiVirusDetection
 
-    Write-Host ""
-    Write-Host "==================== { SeBackUpPrivilege" -ForegroundColor Blue
-    SeBackUpPrivilege
+        Write-Host ""
+        Write-Host "==================== { Windows Installed Hotfixes" -ForegroundColor Blue
+        WindowsHotfixes
 
-    Write-Host ""
-    Write-Host "==================== { SeImpersonatePrivilege" -ForegroundColor Blue
-    SeImpersonatePrivilege
+        Write-Host ""
+        Write-Host "==================== { Windows Hotfix History" -ForegroundColor Blue
+        WindowsHotfixHistory
 
-    Write-Host ""
-    Write-Host "==================== { UnquotedServicePath" -ForegroundColor Blue
-    UnquotedServicePath
+        Write-Host ""
+        Write-Host "==================== { Audit, WEF and LAPS" -ForegroundColor Blue
+        PSAuditWEFLAPS
 
-    Write-Host ""
-    Write-Host "==================== { UnquotedServicePath" -ForegroundColor Blue
-    HiveNightmare
+        Write-Host ""
+        Write-Host "==================== { LSA Protection check" -ForegroundColor Blue
+        LSAProtection
 
-    Write-Host ""
-    Write-Host "==================== { Credentials } ====================" -ForegroundColor Blue
-    Write-Host "==============================================================" -ForegroundColor Blue
+        Write-Host ""
+        Write-Host "==================== { Credential Guard check" -ForegroundColor Blue
+        CredentialGuard
 
-    Write-Host ""
-    Write-Host "==================== { Remote Desktop Credential Manager" -ForegroundColor Blue
-    RemoteDesktopCredentialManager
+        Write-Host ""
+        Write-Host "==================== { WDigest" -ForegroundColor Blue
+        WDigest
 
-    Write-Host ""
-    Write-Host "==================== { Cloud Credentials" -ForegroundColor Blue
-    CloudCredentials
+        Write-Host ""
+        Write-Host "==================== { Cached Winlogon Credentials" -ForegroundColor Blue
+        CachedWinlogonCredentials
 
-    Write-Host ""
-    Write-Host "==================== { OpenVPN Credentials" -ForegroundColor Blue
-    OpenVPNCredentials
+        Write-Host ""
+        Write-Host "==================== { Environment Variables" -ForegroundColor Blue
+        EnvironmentVariables
 
-    Write-Host ""
-    Write-Host "==================== { Wifi Credentials" -ForegroundColor Blue
-    WifiCredentials
+        Write-Host ""
+        Write-Host "==================== { UAC Settings/Bypass" -ForegroundColor Blue
+        UACSettings
 
-    Write-Host ""
-    Write-Host "==================== { OpenSSH Keys" -ForegroundColor Blue
-    OpenSSHKeys
+        Write-Host ""
+        Write-Host "==================== { Spooler/PrintNightmare" -ForegroundColor Blue
+        Spooler
 
-    Write-Host ""
-    Write-Host "==================== { WinVNC Passwords" -ForegroundColor Blue
-    WinVNCPasswords
+        Write-Host ""
+        Write-Host "==================== { Weak registry settings" -ForegroundColor Blue
+        WeakRegistryKeyPermissions
 
-    Write-Host ""
-    Write-Host "==================== { SNMP Passwords" -ForegroundColor Blue
-    SNMPPasswords
+        Write-Host ""
+        Write-Host "==================== { Unattended Files" -ForegroundColor Blue
+        UnattendedFiles
 
-    Write-Host ""
-    Write-Host "==================== { TightVNC Passwords" -ForegroundColor Blue
-    TightVNCPasswords
+        Write-Host ""
+        Write-Host "==================== { Check Directory Permissions" -ForegroundColor Blue
+        CheckDirectoryPermissions
 
-    Write-Host ""
-    Write-Host "==================== { Group Policy Passwords" -ForegroundColor Blue
-    GroupPolicyPasswords
+        Write-Host ""
+        Write-Host "==================== { User Information } ====================" -ForegroundColor Blue
+        Write-Host "==============================================================" -ForegroundColor Blue
+        Write-Host "==================== { Local Groups and directory access" -ForegroundColor Blue
+        UserInformation
 
-    Write-Host ""
-    Write-Host "==================== { SAM SYSTEM Backup" -ForegroundColor Blue
-    SAMSYSTEMBackup
+        Write-Host ""
+        Write-Host "==================== { Net accounts Information" -ForegroundColor Blue
+        NetAccountsInfo
 
-    Write-Host ""
-    Write-Host "==================== { Kerberos Tickets" -ForegroundColor Blue
-    KerberosTickets
+        Write-Host ""
+        Write-Host "==================== { Remote Sessions" -ForegroundColor Blue
+        RemoteSessions
 
-    Write-Host ""
-    Write-Host "==================== { DPAPI Master Keys" -ForegroundColor Blue
-    DPAPIMasterKeys
+        Write-Host ""
+        Write-Host "==================== { Current Sessions" -ForegroundColor Blue
+        CurrentSessions
 
-    Write-Host ""
-    Write-Host "==================== { DPAPI RPC Master Keys" -ForegroundColor Blue
-    DPAPIRPCMasterKeys
+        Write-Host ""
+        Write-Host "==================== { AlwaysInstallElevated" -ForegroundColor Blue
+        AlwaysInstallElevated
 
-    Write-Host ""
-    Write-Host "==================== { Cached Windows Vault Credentials" -ForegroundColor Blue
-    CachedWindowsVaultCredentials
+        Write-Host ""
+        Write-Host "==================== { SeBackUpPrivilege" -ForegroundColor Blue
+        SeBackUpPrivilege
 
-    Write-Host ""
-    Write-Host "==================== { GPP Passwords in files" -ForegroundColor Blue
-    Find-GPHistoryFiles
+        Write-Host ""
+        Write-Host "==================== { SeImpersonatePrivilege" -ForegroundColor Blue
+        SeImpersonatePrivilege
 
-    Write-Host ""
-    Write-Host "==================== { PuTTY } ====================" -ForegroundColor Blue
-    Write-Host "==============================================================" -ForegroundColor Blue
+        Write-Host ""
+        Write-Host "==================== { UnquotedServicePath" -ForegroundColor Blue
+        UnquotedServicePath
 
-    Write-Host ""
-    Write-Host "==================== { PuTTY Credentials" -ForegroundColor Blue
-    PuTTYCredentials
+        Write-Host ""
+        Write-Host "==================== { HiveNightmare" -ForegroundColor Blue
+        HiveNightmare
 
-    Write-Host ""
-    Write-Host "==================== { PuTTY Keys" -ForegroundColor Blue
-    PuTTYKeys
+        Write-Host ""
+        Write-Host "==================== { Credentials } ====================" -ForegroundColor Blue
+        Write-Host "==============================================================" -ForegroundColor Blue
+        Write-Host "==================== { Remote Desktop Credential Manager" -ForegroundColor Blue
+        RemoteDesktopCredentialManager
 
-    Write-Host ""
-    Write-Host "==================== { PuTTY SSH known Hosts" -ForegroundColor Blue
-    PuTTYSSHKnownHosts
+        Write-Host ""
+        Write-Host "==================== { Cloud Credentials" -ForegroundColor Blue
+        CloudCredentials
+
+        Write-Host ""
+        Write-Host "==================== { OpenVPN Credentials" -ForegroundColor Blue
+        OpenVPNCredentials
+
+        Write-Host ""
+        Write-Host "==================== { Wifi Credentials" -ForegroundColor Blue
+        WifiCredentials
+
+        Write-Host ""
+        Write-Host "==================== { OpenSSH Keys" -ForegroundColor Blue
+        OpenSSHKeys
+
+        Write-Host ""
+        Write-Host "==================== { WinVNC Passwords" -ForegroundColor Blue
+        WinVNCPasswords
+
+        Write-Host ""
+        Write-Host "==================== { SNMP Passwords" -ForegroundColor Blue
+        SNMPPasswords
+
+        Write-Host ""
+        Write-Host "==================== { TightVNC Passwords" -ForegroundColor Blue
+        TightVNCPasswords
+
+        Write-Host ""
+        Write-Host "==================== { Group Policy Passwords" -ForegroundColor Blue
+        GroupPolicyPasswords
+
+        Write-Host ""
+        Write-Host "==================== { SAM SYSTEM Backup" -ForegroundColor Blue
+        SAMSYSTEMBackup
+
+        Write-Host ""
+        Write-Host "==================== { Kerberos Tickets" -ForegroundColor Blue
+        KerberosTickets
+
+        Write-Host ""
+        Write-Host "==================== { DPAPI Master Keys" -ForegroundColor Blue
+        DPAPIMasterKeys
+
+        Write-Host ""
+        Write-Host "==================== { DPAPI RPC Master Keys" -ForegroundColor Blue
+        DPAPIRPCMasterKeys
+
+        Write-Host ""
+        Write-Host "==================== { Cached Windows Vault Credentials" -ForegroundColor Blue
+        CachedWindowsVaultCredentials
+
+        Write-Host ""
+        Write-Host "==================== { GPP Passwords in files" -ForegroundColor Blue
+        Find-GPHistoryFiles
+
+        Write-Host ""
+        Write-Host "==================== { PuTTY } ====================" -ForegroundColor Blue
+        Write-Host "==============================================================" -ForegroundColor Blue
+
+        Write-Host ""
+        Write-Host "==================== { PuTTY Credentials" -ForegroundColor Blue
+        PuTTYCredentials
+
+        Write-Host ""
+        Write-Host "==================== { PuTTY Keys" -ForegroundColor Blue
+        PuTTYKeys
+
+        Write-Host ""
+        Write-Host "==================== { PuTTY SSH known Hosts" -ForegroundColor Blue
+        PuTTYSSHKnownHosts
+    } elseif($light) {
+        Write-Host "==================== { Computer Information } ====================" -ForegroundColor Blue
+        Write-Host "==============================================================" -ForegroundColor Blue
+        Write-Host "==================== { KernelInformation" -ForegroundColor Blue
+        KernelInformation
+
+        Write-Host ""
+        Write-Host "==================== { Domain joined test & domain information" -ForegroundColor Blue
+        Test-DomainJoinStatus
+
+        Write-Host ""
+        Write-Host "==================== { Credential Guard check" -ForegroundColor Blue
+        CredentialGuard
+
+        Write-Host ""
+        Write-Host "==================== { Cached Winlogon Credentials" -ForegroundColor Blue
+        CachedWinlogonCredentials
+
+        Write-Host ""
+        Write-Host "==================== { Spooler/PrintNightmare" -ForegroundColor Blue
+        Spooler
+
+        Write-Host ""
+        Write-Host "==================== { User Information } ====================" -ForegroundColor Blue
+        Write-Host "==============================================================" -ForegroundColor Blue
+        Write-Host "==================== { Local Groups and directory access" -ForegroundColor Blue
+        UserInformation
+
+        Write-Host ""
+        Write-Host "==================== { AlwaysInstallElevated" -ForegroundColor Blue
+        AlwaysInstallElevated
+
+        Write-Host ""
+        Write-Host "==================== { SeBackUpPrivilege" -ForegroundColor Blue
+        SeBackUpPrivilege
+
+        Write-Host ""
+        Write-Host "==================== { SeImpersonatePrivilege" -ForegroundColor Blue
+        SeImpersonatePrivilege
+
+        Write-Host ""
+        Write-Host "==================== { UnquotedServicePath" -ForegroundColor Blue
+        UnquotedServicePath
+
+        Write-Host ""
+        Write-Host "==================== { HiveNightmare" -ForegroundColor Blue
+        HiveNightmare
+
+        Write-Host ""
+        Write-Host "==================== { Group Policy Passwords" -ForegroundColor Blue
+        GroupPolicyPasswords
+
+        Write-Host ""
+        Write-Host "==================== { SAM SYSTEM Backup" -ForegroundColor Blue
+        SAMSYSTEMBackup
+    } else {
+        Write-Host "==================== { Computer Information } ====================" -ForegroundColor Blue
+        Write-Host "==============================================================" -ForegroundColor Blue
+        Write-Host "==================== { KernelInformation" -ForegroundColor Blue
+        KernelInformation
+
+        Write-Host ""
+        Write-Host "==================== { Domain joined test & domain information" -ForegroundColor Blue
+        Test-DomainJoinStatus
+
+        Write-Host ""
+        Write-Host "==================== { Credential Guard check" -ForegroundColor Blue
+        CredentialGuard
+
+        Write-Host ""
+        Write-Host "==================== { Cached Winlogon Credentials" -ForegroundColor Blue
+        CachedWinlogonCredentials
+
+        Write-Host ""
+        Write-Host "==================== { Spooler/PrintNightmare" -ForegroundColor Blue
+        Spooler
+        Write-Host ""
+        Write-Host "==================== { User Information } ====================" -ForegroundColor Blue
+        Write-Host "==============================================================" -ForegroundColor Blue
+        Write-Host "==================== { Local Groups and directory access" -ForegroundColor Blue
+        UserInformation
+
+        Write-Host ""
+        Write-Host "==================== { AlwaysInstallElevated" -ForegroundColor Blue
+        AlwaysInstallElevated
+
+        Write-Host ""
+        Write-Host "==================== { SeBackUpPrivilege" -ForegroundColor Blue
+        SeBackUpPrivilege
+
+        Write-Host ""
+        Write-Host "==================== { SeImpersonatePrivilege" -ForegroundColor Blue
+        SeImpersonatePrivilege
+
+        Write-Host ""
+        Write-Host "==================== { UnquotedServicePath" -ForegroundColor Blue
+        UnquotedServicePath
+
+        Write-Host ""
+        Write-Host "==================== { HiveNightmare" -ForegroundColor Blue
+        HiveNightmare
+
+        Write-Host ""
+        Write-Host "==================== { Group Policy Passwords" -ForegroundColor Blue
+        GroupPolicyPasswords
+
+        Write-Host ""
+        Write-Host "==================== { SAM SYSTEM Backup" -ForegroundColor Blue
+        SAMSYSTEMBackup
+    }
 }
 
 function Invoke-ServerChecks {
